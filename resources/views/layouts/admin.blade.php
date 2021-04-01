55901c3fe08c86b90e7130f6ee3fdf84d4417269
Amohelang Thoabala <38657357+amohelangthoabala@users.noreply.github.com>
2021-04-01 14:39:01 +0200

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,16 +18,16 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}} " rel="stylesheet">
-  <link href="{{asset('assets/vendor/icofont/icofont.min.css')}} " rel="stylesheet">
-  <link href="{{asset('assets/vendor/boxicons/css/boxicons.min.css')}} " rel="stylesheet">
-  <link href="{{asset('assets/vendor/venobox/venobox.css')}} " rel="stylesheet">
-  <link href="{{asset('assets/vendor/line-awesome/css/line-awesome.min.css')}} " rel="stylesheet">
-  <link href="{{asset('assets/vendor/owl.carousel/assets/owl.carousel.min.css')}} " rel="stylesheet">
-  <link href="{{asset('assets/vendor/aos/aos.css')}} " rel="stylesheet">
+  <link href="{{secure_asset('assets/vendor/bootstrap/css/bootstrap.min.css')}} " rel="stylesheet">
+  <link href="{{secure_asset('assets/vendor/icofont/icofont.min.css')}} " rel="stylesheet">
+  <link href="{{secure_asset('assets/vendor/boxicons/css/boxicons.min.css')}} " rel="stylesheet">
+  <link href="{{secure_asset('assets/vendor/venobox/venobox.css')}} " rel="stylesheet">
+  <link href="{{secure_asset('assets/vendor/line-awesome/css/line-awesome.min.css')}} " rel="stylesheet">
+  <link href="{{secure_asset('assets/vendor/owl.carousel/assets/owl.carousel.min.css')}} " rel="stylesheet">
+  <link href="{{secure_asset('assets/vendor/aos/aos.css')}} " rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="/assets/css/style.css" rel="stylesheet">
+  <link href="{{secure_asset('/assets/css/style.css')}} " rel="stylesheet">
  <!-- =======================================================
   * Template Name: Serenity - v2.2.0
   * Template URL: https://bootstrapmade.com/serenity-bootstrap-corporate-template/
